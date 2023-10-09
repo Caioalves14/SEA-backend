a61f499f5f1c43cb1566beeaaa2d1a457ed30bec
Caioalves14 <caio.alvesrex@gmail.com>
2023-10-09 11:18:50 -0300

    Configurando minha conexão com o banco

--- src/main/java/com/example/sea/SamgranApplication.java
+++ src/main/java/com/example/sea/SamgranApplication.java
@@ -2,10 +2,8 @@ package com.example.sea;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 @SpringBootApplication
-//@SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
 public class SamgranApplication {
     public static void main(String[] args) {
 
